76436c304818979235d769129eec2cb6802db2f3
Abdullah Wasiq <97777254+aftwasiq@users.noreply.github.com>
2025-07-22 20:14:32 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define SHELL_BUFFER 1
+#define PATH_MAX 4096
+#define MAGENTA "\033[35m"
+#define DEFAULT "\033[0m"
+
+struct termios original;
+
+const char *getattr_error = "Unable to get terminal attributes.\n";
+const char *setattr_error = "Unable to set terminal attributes.\n";
+const char *qsh = "$ ";
+
+void enterRawMode(void) {
+    if (tcgetattr(STDIN_FILENO, &original) == -1) {
+        write(STDERR_FILENO, getattr_error, strlen(getattr_error)); 
+        return;
+    }
+
+    struct termios raw;
+
+    if (tcgetattr(STDIN_FILENO, &raw) == -1) {
+        write(STDERR_FILENO, getattr_error, strlen(getattr_error));
+        return;
+    }
+        
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        write(STDERR_FILENO, setattr_error, strlen(setattr_error));
+        return;
+    }
+
+}
+
+void restoreOriginalMode(void) {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original) == -1) {
+        write(STDERR_FILENO, setattr_error, strlen(setattr_error)); 
+    }
+}
+
+void welcomeMessage(void) {
+    const char *welcome = "\n[qsh] Welcome! Use 'help' for assistance";
+    const char *user = "\n[qsh] User: ";
+    const char *name = getenv("USER");
+
+    if (!name) {
+        name = "UNKNOWN";
+    }
+
+    write(STDOUT_FILENO, welcome, strlen(welcome));
+    write(STDOUT_FILENO, user, strlen(user));
+    write(STDOUT_FILENO, name, strlen(name));
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+void shellPrompt(void) {
+    char home[100];
+    const char *name = getenv("USER");
+    snprintf(home, sizeof(home), "/home/%s/", name);
+
+    char current_directory[PATH_MAX];
+    ssize_t bytes;
+
+    if (getcwd(current_directory, sizeof(current_directory)) != NULL) {
+        if (strncmp(current_directory, home, strlen(home)) == 0) {
+            char *msg = (current_directory + strlen(home));
+
+            write(STDOUT_FILENO, "[qsh] ", 6);
+            write(STDOUT_FILENO, MAGENTA, strlen(MAGENTA));
+            
+            write(STDOUT_FILENO, name, strlen(name));
+            write(STDOUT_FILENO, "@", 1);
+            write(STDOUT_FILENO, "~", 1);
+            write(STDOUT_FILENO, msg, strlen(msg));
+            
+            write(STDOUT_FILENO, DEFAULT, strlen(DEFAULT));
+            write(STDOUT_FILENO, qsh, strlen(qsh));
+        }
+    } else {
+        return;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    enterRawMode();
+
+    const char *newline = "\r\n";
+    int fd = open("shell_art.txt", O_RDONLY);
+
+    if (fd == -1) {
+        return 1;
+    }
+
+    char buf[SHELL_BUFFER];
+    ssize_t bytes;
+    
+    restoreOriginalMode();
+
+    while (bytes = read(fd, &buf, SHELL_BUFFER) > 0) {
+        if (buf[0] == '\n') {
+            write(STDOUT_FILENO, newline, strlen(newline));
+        } else {
+            write(STDOUT_FILENO, &buf, SHELL_BUFFER);
+        }
+    }
+    
+    welcomeMessage();
+    enterRawMode();
+
+    write(STDOUT_FILENO, newline, strlen(newline)); 
+
+    shellPrompt();
+
+    while (bytes = read(STDIN_FILENO, &buf, SHELL_BUFFER) > 0) {
+        if (buf[0] == '\r') { 
+            write(STDOUT_FILENO, newline, strlen(newline));
+            shellPrompt();
+        } else {
+            write(STDOUT_FILENO, &buf, SHELL_BUFFER);
+        }
+    }
+
+    atexit(&restoreOriginalMode);
+
+    return 0;
+}
+
+
+
+
