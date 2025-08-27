0670de4959a9c77ada334c9eeb2dfb98ab2f0531
Abdullah Wasiq <97777254+aftwasiq@users.noreply.github.com>
2025-08-27 00:12:50 -0400

    Update executor.c

--- executor.c
+++ executor.c
@@ -24,20 +24,21 @@ typedef struct {
 void restoreOriginalMode(void);
 
 int execute(COMMAND *cmd) {
-    const char *pwd_failure = "Unable to list items.\n"; 
-    const char *cd_failure = "The provided directories do not exist.\n";
-    const char *mkdir_failure = "Unable to create new directory.\n";
-    const char *exists = "Directory already exists.\n";
+    const char *pwd_failure = "Unable to list items.\n\r"; 
+    const char *cd_failure = "The provided directories do not exist.\n\r";
+    const char *mkdir_failure = "Unable to create new directory.\n\r";
+    const char *exists = "Directory already exists.\n\r";
 
     char current_directory[PATH_MAX];
-
+    
     if (strcmp("cd", cmd->arg) == 0) {
         if (syscall(SYS_chdir, cmd->argv[1]) == -1) {
             write(STDERR_FILENO, cd_failure, strlen(cd_failure));
-        } 
+        }   
     } 
 
     if (strcmp("ls", cmd->arg) == 0) {
+        /*
         int dir = open(".", O_RDONLY | O_DIRECTORY);
         if (dir == -1) {
             write(STDERR_FILENO, pwd_failure, strlen(pwd_failure));
@@ -82,7 +83,7 @@ int execute(COMMAND *cmd) {
                     new->argv[2] = NULL;
                     new->argc = 2;
 
-                    execute(new);
+                    execute(new, tokens);
 
                     free(new->argv[1]);
                     free(new->argv);
@@ -94,10 +95,12 @@ int execute(COMMAND *cmd) {
         }
 
         close(dir);
+    */
     }
 
     if (strcmp("help", cmd->arg) == 0) {
-        const char *helper = "Commands:\nls\ncd [path]\nmkdir [name]\nexit\npwd\n";        
+        const char *helper = "Commands:\n\rls\n\rcd [path]\n\rmkdir [name]\n\rexit\n\rpwd\r";
+
         write(STDOUT_FILENO, helper, strlen(helper));
     }
 
@@ -119,10 +122,13 @@ int execute(COMMAND *cmd) {
     if (strcmp("pwd", cmd->arg) == 0) {
         if (getcwd(current_directory, sizeof(current_directory)) != NULL) {
             write(STDOUT_FILENO, current_directory, strlen(current_directory));
+            write(STDOUT_FILENO, "\r\n", 2);
         } else {
             write(STDERR_FILENO, pwd_failure, strlen(pwd_failure));
         }
     }
-
+    
+    free_commands(cmd);
     return 0;
 }
+
